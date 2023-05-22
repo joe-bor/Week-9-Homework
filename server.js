ab1a5462f2eba48e7140be9ace005e197d3cf5a7
joe-bor <kimborlongan_08@yahoo.com>
2023-05-22 13:11:04 -0700

    Tip Calculaor express aplication created.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+/* R O U T E S */
+
 app.get("/", (req, res) => {
   res.send("This is the root");
 });
@@ -10,6 +12,26 @@ app.get("/greeting/:name?", (req, res) => {
   if (!req.params.name) res.send("Hello Stranger");
   res.send(`Hello there ${req.params.name}, nice to see you!`);
 });
+
+app.get("/tip/:total?/:tipPercentage?", (req, res) => {
+  let tip =
+    parseInt(req.params.total) * (parseInt(req.params.tipPercentage) / 100);
+
+  //check if both params are given
+  if (!req.params.total || !req.params.tipPercentage) {
+    res.send("/tip/:total/:tipPercentage both params are required");
+  } else if (
+    Number.isFinite(req.params.total) ||
+    Number.isFinite(req.params.tipPercentage)
+  ) {
+    res.send("Both params must be a finite number!");
+    console.log("both params must be finite");
+  } else {
+    res.send(`tip = ${tip}`);
+  }
+});
+
+/* END */
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
